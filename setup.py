8dac66293dfbfbb9dc44f59090045a5dbc7a9b8f
SkafteNicki <skaftenicki@gmail.com>
2022-05-03 09:08:06 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='laplaceae',
+   version='0.1',
+   description='Laplace auto encoder',
+   author='foo',
+   author_email='foomail@foo.com',
+   packages=['src'],  #same as name
+)
